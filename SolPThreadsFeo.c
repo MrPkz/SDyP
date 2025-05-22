b8535182523f472049be4efd9a4b37d903d6f308
Ramiro M. <ramiro.madera@alu.ing.unlp.edu.ar>
2025-05-22 20:54:35 -0300

    Intenté ver si con la división equitativa funcionaba, pues no

---
+++ SolPThreadsFeo.c
@@ -0,0 +1,443 @@
+// Compilar:
+//		gcc -o n_body_simple_NOGL n_body_simple.c -lm
+// Ejecutar:
+//		./n_body_simple_NOGL <nro de cuerpos> <DT> <Pasos>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <math.h>
+#include <sys/time.h>
+#include <pthread.h>
+
+//
+// Para tiempo de ejecucion
+//
+
+double dwalltime()
+{
+    double sec;
+    struct timeval tv;
+
+    gettimeofday(&tv, NULL);
+    sec = tv.tv_sec + tv.tv_usec / 1000000.0;
+    return sec;
+}
+
+double tIni, tFin, tTotal;
+
+//
+// Constantes para Algoritmo de gravitacion
+//
+#define PI (3.141592653589793)
+#define G 6.673e-11
+#define ESTRELLA 0
+#define POLVO 1
+#define H2 2 // Hidrogeno molecular
+
+// ===============
+// ===== CPU =====
+// ===============
+
+//
+// Estructuras y variables para Algoritmo de gravitacion
+//
+typedef struct cuerpo cuerpo_t;
+struct cuerpo
+{
+    float masa;
+    float px;
+    float py;
+    float pz;
+    float vx;
+    float vy;
+    float vz;
+    float r;
+    float g;
+    float b;
+    int cuerpo;
+};
+
+float *fuerza_totalX, *fuerza_totalY, *fuerza_totalZ;
+float toroide_alfa;
+float toroide_theta;
+float toroide_incremento;
+float toroide_lado;
+float toroide_r;
+float toroide_R;
+
+cuerpo_t *cuerpos;
+int delta_tiempo = 1.0f; // Intervalo de tiempo, longitud de un paso
+int dt = 1.0f;
+int pasos;
+int N,T;
+
+//
+// Funciones para Algoritmo de gravitacion
+//
+
+void calcularFuerzas(cuerpo_t *cuerpos, int N, int bloque, int tid){
+	int cuerpo1, cuerpo2;
+    int indice1,indice2;
+	float dif_X, dif_Y, dif_Z;
+	float distancia;
+	float F;
+	for (cuerpo1 = tid*bloque; cuerpo1 < (tid+1)*(bloque); cuerpo1++){
+        indice1=cuerpo1*T+tid;
+		for(cuerpo2 = cuerpo1 + 1; cuerpo2<N ; cuerpo2++){
+            // printf("PAREJA %d %d \n",cuerpo1,cuerpo2);
+            indice2=cuerpo2*T+tid;
+			if ( (cuerpos[cuerpo1].px == cuerpos[cuerpo2].px) && (cuerpos[cuerpo1].py == cuerpos[cuerpo2].py) && (cuerpos[cuerpo1].pz == cuerpos[cuerpo2].pz))
+				continue;
+
+			dif_X = cuerpos[cuerpo2].px - cuerpos[cuerpo1].px;
+			dif_Y = cuerpos[cuerpo2].py - cuerpos[cuerpo1].py;
+			dif_Z = cuerpos[cuerpo2].pz - cuerpos[cuerpo1].pz;
+
+            // if(cuerpo2=tid*bloque+1) printf("%.2f %.2f\n",dif_X,dif_Y);
+				
+			distancia = sqrt(dif_X*dif_X + dif_Y*dif_Y + dif_Z*dif_Z);
+
+            // if(cuerpo2==1) printf("DIST %.6f \n",distancia);
+
+			F = (G*cuerpos[cuerpo1].masa*cuerpos[cuerpo2].masa)/(distancia*distancia);
+
+            // if(cuerpo2==1) printf("MASAS %.6f %.6f \n",cuerpos[cuerpo1].masa,cuerpos[cuerpo2].masa);
+
+            // if(cuerpo2==1) printf("FUERZA %.15f \n",F);
+
+			dif_X *= F;
+			dif_Y *= F;
+			dif_Z *= F;
+
+            // if(cuerpo2=tid*bloque+1) printf("COORD %.2f %.2f\n",dif_X,dif_Y);
+
+			fuerza_totalX[indice1] += dif_X;
+			fuerza_totalY[indice1] += dif_Y;
+			fuerza_totalZ[indice1] += dif_Z;
+
+            // if(cuerpo2==1) printf("%d %.15f\n",cuerpo1,fuerza_totalX[indice1]);
+
+			fuerza_totalX[indice2] -= dif_X;
+			fuerza_totalY[indice2] -= dif_Y;
+			fuerza_totalZ[indice2] -= dif_Z;
+
+            // if(cuerpo2==1) printf("%d %.15f\n",cuerpo2,fuerza_totalX[indice2]);
+		}
+	}
+
+   
+
+    return;
+}
+
+void moverCuerpos(cuerpo_t *cuerpos, int N, int bloque, int tid,int dt){
+ int cuerpo,index,base;
+	for(cuerpo = tid*bloque; cuerpo < (tid+1)*bloque; cuerpo++){
+
+        base=cuerpo*T;
+
+        // if(fuerza_totalX[base]!=0) printf("%.2F\n",fuerza_totalX[base]);
+
+        for(index = 1; index<T; index++){
+            fuerza_totalX[base]+=fuerza_totalX[base+index];
+            fuerza_totalY[base]+=fuerza_totalY[base+index];
+            fuerza_totalX[base+index] = 0.0;
+            fuerza_totalY[base+index] = 0.0;
+            fuerza_totalZ[base+index] = 0.0;
+            // fuerza_totalZ[base]+=fuerza_totalZ[base+index];
+        }
+
+        // printf("%d %d \n",cuerpo,tid);
+        // if(cuerpo==0) printf("Forza %.15f\n",fuerza_totalX[base]);
+
+		fuerza_totalX[base] *= 1/cuerpos[cuerpo].masa;
+		fuerza_totalY[base] *= 1/cuerpos[cuerpo].masa;
+		//fuerza_totalZ[base] *= 1/cuerpos[cuerpo].masa;
+
+        // if(cuerpo==0) printf("Acel %.15f\n",fuerza_totalX[base]);
+
+		cuerpos[cuerpo].vx += fuerza_totalX[base]*dt;
+		cuerpos[cuerpo].vy += fuerza_totalY[base]*dt;
+		//cuerpos[cuerpo].vz += fuerza_totalZ[base]*dt;
+
+        // if(cuerpo==0) printf("Veloc %d %.15f\n",dt,cuerpos[cuerpo].vx);
+
+		cuerpos[cuerpo].px += cuerpos[cuerpo].vx *dt;
+		cuerpos[cuerpo].py += cuerpos[cuerpo].vy *dt;
+		//cuerpos[cuerpo].pz += cuerpos[cuerpo].vz *dt;
+
+        // if(cuerpo==2) printf("%.15f\n",cuerpos[cuerpo].px);
+
+        // for(index = 0; index<T; index++){
+            fuerza_totalX[base] = 0.0;
+            fuerza_totalY[base] = 0.0;
+            fuerza_totalZ[base] = 0.0;
+        // }
+
+    }
+}
+
+pthread_barrier_t barrier;
+
+// void funcionThreads(cuerpo_t *cuerpos, int N, int dt)
+void *funcionThreads(void *arg)
+{
+    int tid=(int)arg;
+    int bloque=N/(2*T); //Cada thread tiene 2 bloquecitos de N/4T. Suma N/2T en total
+    int bloque2=N/T;
+    int cuerpo1, cuerpo2;
+    float dif_X, dif_Y, dif_Z;
+    float distancia;
+    float F;
+    int cuerpo;
+    
+
+    int paso=0;
+    while(paso<pasos){
+        
+        calcularFuerzas(cuerpos,N,bloque2,tid);
+
+        //Barrera
+
+        //Calculo de movimientos
+        // printf("1\n");
+        pthread_barrier_wait(&barrier);
+        // printf("2\n");
+
+        moverCuerpos(cuerpos,N,bloque2,tid,dt);
+
+        //Otra barrera
+
+        // printf("3\n");
+        pthread_barrier_wait(&barrier);
+        // printf("4\n");
+
+        paso++;
+    }
+}
+
+void gravitacionCPU(cuerpo_t *cuerpos, int N, int dt)
+{
+    pthread_t misThreads[T];
+    int threads_ids[T];
+
+    pthread_barrier_init(&barrier, NULL, T);
+
+    for(int id=0;id<T;id++){
+		threads_ids[id]=id;
+		pthread_create(&misThreads[id],NULL,&funcionThreads,(void*)id);
+	}
+	
+	for(int id=0; id<T;id++){
+		pthread_join(misThreads[id],NULL);
+	}
+
+    pthread_barrier_destroy(&barrier);
+
+    // calcularFuerzas(cuerpos, N, dt);
+    // moverCuerpos(cuerpos, N, dt);
+}
+
+void inicializarEstrella(cuerpo_t *cuerpo, int i, double n)
+{
+
+    cuerpo->masa = 0.001 * 8;
+
+    if ((toroide_alfa + toroide_incremento) >= 2 * M_PI)
+    {
+        toroide_alfa = 0;
+        toroide_theta += toroide_incremento;
+    }
+    else
+    {
+        toroide_alfa += toroide_incremento;
+    }
+
+    cuerpo->px = (toroide_R + toroide_r * cos(toroide_alfa)) * cos(toroide_theta);
+    cuerpo->py = (toroide_R + toroide_r * cos(toroide_alfa)) * sin(toroide_theta);
+    cuerpo->pz = toroide_r * sin(toroide_alfa);
+
+    cuerpo->vx = 0.0;
+    cuerpo->vy = 0.0;
+    cuerpo->vz = 0.0;
+
+    cuerpo->r = 1.0; //(double )rand()/(RAND_MAX+1.0);
+    cuerpo->g = 1.0; //(double )rand()/(RAND_MAX+1.0);
+    cuerpo->b = 1.0; //(double )rand()/(RAND_MAX+1.0);
+}
+
+void inicializarPolvo(cuerpo_t *cuerpo, int i, double n)
+{
+
+    cuerpo->masa = 0.001 * 4;
+
+    if ((toroide_alfa + toroide_incremento) >= 2 * M_PI)
+    {
+        toroide_alfa = 0;
+        toroide_theta += toroide_incremento;
+    }
+    else
+    {
+        toroide_alfa += toroide_incremento;
+    }
+
+    cuerpo->px = (toroide_R + toroide_r * cos(toroide_alfa)) * cos(toroide_theta);
+    cuerpo->py = (toroide_R + toroide_r * cos(toroide_alfa)) * sin(toroide_theta);
+    cuerpo->pz = toroide_r * sin(toroide_alfa);
+
+    cuerpo->vx = 0.0;
+    cuerpo->vy = 0.0;
+    cuerpo->vz = 0.0;
+
+    cuerpo->r = 1.0; //(double )rand()/(RAND_MAX+1.0);
+    cuerpo->g = 0.0; //(double )rand()/(RAND_MAX+1.0);
+    cuerpo->b = 0.0; //(double )rand()/(RAND_MAX+1.0);
+}
+
+void inicializarH2(cuerpo_t *cuerpo, int i, double n)
+{
+
+    cuerpo->masa = 0.001;
+
+    if ((toroide_alfa + toroide_incremento) >= 2 * M_PI)
+    {
+        toroide_alfa = 0;
+        toroide_theta += toroide_incremento;
+    }
+    else
+    {
+        toroide_alfa += toroide_incremento;
+    }
+
+    cuerpo->px = (toroide_R + toroide_r * cos(toroide_alfa)) * cos(toroide_theta);
+    cuerpo->py = (toroide_R + toroide_r * cos(toroide_alfa)) * sin(toroide_theta);
+    cuerpo->pz = toroide_r * sin(toroide_alfa);
+
+    cuerpo->vx = 0.0;
+    cuerpo->vy = 0.0;
+    cuerpo->vz = 0.0;
+
+    cuerpo->r = 1.0; //(double )rand()/(RAND_MAX+1.0);
+    cuerpo->g = 1.0; //(double )rand()/(RAND_MAX+1.0);
+    cuerpo->b = 1.0; //(double )rand()/(RAND_MAX+1.0);
+}
+
+void inicializarCuerpos(cuerpo_t *cuerpos, int N)
+{
+    int cuerpo;
+    double n = N;
+
+    toroide_alfa = 0.0;
+    toroide_theta = 0.0;
+    toroide_lado = sqrt(N);
+    toroide_incremento = 2 * M_PI / toroide_lado;
+    toroide_r = 1.0;
+    toroide_R = 2 * toroide_r;
+
+    // srand(time(NULL));
+    srand(0);
+
+    for (cuerpo = 0; cuerpo < N; cuerpo++)
+    {
+
+        fuerza_totalX[cuerpo] = 0.0;
+        fuerza_totalY[cuerpo] = 0.0;
+        fuerza_totalZ[cuerpo] = 0.0;
+
+        cuerpos[cuerpo].cuerpo = (rand() % 3);
+
+        if (cuerpos[cuerpo].cuerpo == ESTRELLA)
+        {
+            inicializarEstrella(&cuerpos[cuerpo], cuerpo, n);
+        }
+        else if (cuerpos[cuerpo].cuerpo == POLVO)
+        {
+            inicializarPolvo(&cuerpos[cuerpo], cuerpo, n);
+        }
+        else if (cuerpos[cuerpo].cuerpo == H2)
+        {
+            inicializarH2(&cuerpos[cuerpo], cuerpo, n);
+        }
+    }
+
+    cuerpos[0].masa = 2.0e2;
+    cuerpos[0].px = 0.0;
+    cuerpos[0].py = 0.0;
+    cuerpos[0].pz = 0.0;
+    cuerpos[0].vx = -0.000001;
+    cuerpos[0].vy = -0.000001;
+    cuerpos[0].vz = 0.0;
+
+    cuerpos[1].masa = 1.0e1;
+    cuerpos[1].px = -1.0;
+    cuerpos[1].py = 0.0;
+    cuerpos[1].pz = 0.0;
+    cuerpos[1].vx = 0.0;
+    cuerpos[1].vy = 0.0001;
+    cuerpos[1].vz = 0.0;
+}
+
+void finalizar(void)
+{
+    free(cuerpos);
+    free(fuerza_totalX);
+    free(fuerza_totalY);
+    free(fuerza_totalZ);
+}
+
+int main(int argc, char *argv[])
+{
+
+    if (argc < 4)
+    {
+        printf("Ejecutar: %s <nro. de cuerpos> <DT> <pasos>\n", argv[0]);
+        return -1;
+    }
+
+    N = atoi(argv[1]);            // 512 1024 2048 4096
+    delta_tiempo = atof(argv[2]); // 200
+    dt=delta_tiempo;
+    pasos = atoi(argv[3]);        // 1000
+    T = atoi(argv[4]);            // 4 8
+
+    cuerpos = (cuerpo_t *)malloc(sizeof(cuerpo_t) * N);
+    fuerza_totalX = (float *)malloc(sizeof(float) * T * N);
+    fuerza_totalY = (float *)malloc(sizeof(float) * T * N);
+    fuerza_totalZ = (float *)malloc(sizeof(float) * T * N);
+
+    inicializarCuerpos(cuerpos, N);
+
+    for (int i = 0; i < N*T; i++)
+    {
+        fuerza_totalX[i] = 0.0;
+        fuerza_totalY[i] = 0.0;
+        fuerza_totalZ[i] = 0.0;
+    }
+
+    for(int i=0; i<N; i++)
+		printf("Posicion inicial de cuerpo %d: %.15f %.15f \n",i,cuerpos[i].px,cuerpos[i].py);
+	// printf("Posicion inicial de cuerpo 1: %.15f %.2f \n",cuerpos[1].px,cuerpos[1].py);
+    // printf("Posicion inicial de cuerpo 2: %.15f %.2f \n",cuerpos[2].px,cuerpos[2].py);
+
+    tIni = dwalltime();
+
+    // int paso;
+    // for (paso = 0; paso < pasos; paso++)
+    // {
+        gravitacionCPU(cuerpos, N, delta_tiempo);
+    // }
+
+    tFin = dwalltime();
+    tTotal = tFin - tIni;
+
+    printf("Tiempo en segundos: %f\n", tTotal);
+    for(int i=0; i<N; i++)
+		printf("Posicion final de cuerpo %d: %.15f %.15f \n",i,cuerpos[i].px,cuerpos[i].py);
+    // printf("Posicion final de cuerpo 1: %.15f %.15f \n",cuerpos[1].px,cuerpos[1].py);
+    // printf("Posicion final de cuerpo 2: %.15f %.15f \n",cuerpos[2].px,cuerpos[2].py);
+
+    finalizar();
+    return (0);
+}
